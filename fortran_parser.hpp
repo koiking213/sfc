b386fb404b8c12fe06da903289b4a7c4639ba8ef
Fujii Yuya <koiking213@gmail.com>
2017-12-27 01:25:21 +0900

    implemented use statement only part

--- fortran_parser.hpp
+++ fortran_parser.hpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-//#define BOOST_SPIRIT_DEBUG
+#define BOOST_SPIRIT_DEBUG
 #include <boost/spirit/include/support_utree.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix.hpp>
@@ -23,18 +23,27 @@ namespace client {
       import_stmt,
       module,
       module_name,
+      module_nature,
+      only_part,
+      only_list,
+      one_only,
+      generic_spec,
       comment;
     qi::rule<Iterator, std::string()> letter, digit, underscore, blank;
     qi::rule<Iterator, std::string()> alphanumeric_character;
-    qi::rule<Iterator, std::string()> Arrow;
-    qi::rule<Iterator, spirit::utree::list_type()> PROGRAM, END, USE;
+    qi::rule<Iterator, std::string()> Arrow, Doublesemi;
+    qi::rule<Iterator, spirit::utree::list_type()> PROGRAM, END, USE, INTRINSIC, NON_INTRINSIC, ONLY;
   public:
     fortran_parser(bool fixed=false) : fortran_parser::base_type(top)
     {
       PROGRAM = qi::as<std::string>()["program"];
       END = qi::as<std::string>()["end"];
       USE = qi::as<std::string>()["use"];
+      INTRINSIC = qi::as<std::string>()["intrinsic"];
+      NON_INTRINSIC = qi::as<std::string>()["non_intrinsic"];
+      ONLY = qi::as<std::string>()["only"];
       Arrow = qi::as<std::string>()["=>"];
+      Doublesemi = qi::as<std::string>()["::"];
 
       if (fixed) {
 	blank = "";
@@ -65,12 +74,22 @@ namespace client {
 
       // doing
       use_stmt = qi::skip(qi::ascii::blank)[USE] >>
-	//-( -(',' >> modul_enature) >> qi::skip(qi::ascii:blank)DOUBLESEMI)
-	qi::omit[blank] >> name >> -(qi::skip(qi::ascii::blank)[','] >> qi::omit[*qi::ascii::blank] >> rename_list);
+	-( -(',' >> qi::skip(qi::ascii::blank)[module_nature])
+	   >> qi::skip(qi::ascii::blank)[Doublesemi]) >>
+	qi::omit[blank] >> name >> -(qi::skip(qi::ascii::blank)[','] >> qi::omit[*qi::ascii::blank]
+				     >> (rename_list|only_part));
+
+      module_nature = INTRINSIC | NON_INTRINSIC;
+
+      only_part = ONLY >> qi::skip(qi::ascii::blank)[':'] >> only_list;
+      only_list = (generic_spec | rename | name) % qi::char_(',');
+      
+      // todo
+      rename_list = rename % qi::char_(',');
+      rename = qi::skip(qi::ascii::blank)[name] >> qi::skip(qi::ascii::blank)[Arrow] >> qi::skip(qi::ascii::blank)[name];
 
       // todo
-      rename_list = rename % ',';
-      rename = name >> qi::skip(qi::ascii::blank)[Arrow] >> qi::skip(qi::ascii::blank)[name];
+      generic_spec = "this is generic-spec" >> name;
       
       // todo
       import_stmt = "this is import statement" >> name >> name;
