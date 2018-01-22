4548fbece484411ce282e97a69d0ba420e214b3e
koiking213 <koiking213@gmail.com>
2018-01-22 15:59:39 +0900

    implemented expression stringizer but compilation is failed

--- test_parser.cpp
+++ test_parser.cpp
@@ -9,26 +9,39 @@
 #include <boost/spirit/include/phoenix_stl.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/io.hpp>
+#include <boost/variant/recursive_wrapper.hpp>
+#include <boost/variant/static_visitor.hpp>
+#include <boost/variant/apply_visitor.hpp>
 
 namespace ast {
-  struct add;
-  struct sub;
-  struct mul;
-  struct div;
+  enum struct operators {
+    add, sub, mul, div
+  };
 
-  template <class Op>
+  template <operators Op>
   struct binary_op;
+
+  char const* to_string(operators op)
+  {
+    switch(op) {
+    case operators::add: return "+";
+    case operators::sub: return "-";
+    case operators::mul: return "*";
+    case operators::div: return "/";
+    default: return nullptr;
+    }
+  }
 
   using Expression = boost::variant<
     int,
     std::string,
-    boost::recursive_wrapper< binary_op< add > >,
-    boost::recursive_wrapper< binary_op< sub > >,
-    boost::recursive_wrapper< binary_op< mul > >,
-    boost::recursive_wrapper< binary_op< div > >
+    boost::recursive_wrapper< binary_op< operators::add > >,
+    boost::recursive_wrapper< binary_op< operators::sub > >,
+    boost::recursive_wrapper< binary_op< operators::mul > >,
+    boost::recursive_wrapper< binary_op< operators::div > >
     >;
 
-  template <class Op>
+  template <operators Op>
   struct binary_op
   {
     Expression lhs;
@@ -38,7 +51,45 @@ namespace ast {
       lhs( lhs_ ), rhs( rhs_ )
     { }
   };
+
+  struct stringizer : public boost::static_visitor<std::string> {
+    std::string operator()(int const constant) const
+    {
+      std::cout << "[DEBUG] int:" << constant << std::endl;
+      return std::to_string(constant);
+    }
+    std::string operator()(std::string const var) const
+    {
+      std::cout << "[DEBUG] name:" << var << std::endl;
+      return var;
+    }
+    template<operators Binary_op>
+    std::string operator()(binary_op<Binary_op> const& op) const
+    {
+      std::cout << "[DEBUG] to_string(Binary_op):" << to_string(Binary_op) << std::endl;
+      return "("
+    	 + boost::apply_visitor(stringizer(), op.lhs)
+    	 + to_string(Binary_op)
+    	 + boost::apply_visitor(stringizer(), op.rhs)
+    	 + ")";
+    }
+  };
+  
+   std::string stringize(Expression const& expr)
+   {
+     return boost::apply_visitor(stringizer(), expr);
+   }
+
+  template<operators Op>
+  auto make_binary_operator()
+  {
+    return boost::phoenix::bind(
+				[](auto const& lhs, auto const& rhs){
+				  return binary_op<Op>(lhs, rhs);
+				}, boost::spirit::_val, boost::spirit::_1 );
+  }
 }
+
 
 namespace qi = boost::spirit::qi;
 namespace spirit = boost::spirit;
@@ -135,7 +186,6 @@ BOOST_FUSION_ADAPT_STRUCT (
 			   (ast::Expression, rhs)
 			   )
 
-
 template <typename Iterator>
 struct test_parser : qi::grammar<Iterator, Program(), ascii::blank_type>
 {
@@ -212,13 +262,13 @@ struct test_parser : qi::grammar<Iterator, Program(), ascii::blank_type>
     // todo
     expr = add_operand [_val = _1]
       >> *(
-	   ('+' >> add_operand [phx::construct<ast::binary_op<ast::add> >(_val, _1)])
-	   | ('-' >> add_operand [phx::construct<ast::binary_op<ast::sub> >(_val, _1)])
+	   ('+' >> add_operand [_val = ast::make_binary_operator<ast::operators::add>()])
+	   | ('-' >> add_operand [_val = ast::make_binary_operator<ast::operators::sub>()])
 	   );
     add_operand = mult_operand [_val = _1]
       >> *(
-	   ('*' >> mult_operand [phx::construct<ast::binary_op<ast::mul> >(_val, _1)])
-	   | ('/' >> mult_operand [phx::construct<ast::binary_op<ast::div> >(_val, _1)])
+	   ('*' >> mult_operand [phx::construct<ast::binary_op<ast::operators::mul> >(_val, _1)])
+	   | ('/' >> mult_operand [phx::construct<ast::binary_op<ast::operators::div> >(_val, _1)])
 	   );
     mult_operand = level_1_expr.alias();
     level_1_expr = primary.alias();
@@ -291,15 +341,29 @@ void print_type_declaration_statement(Type_specification &s)
   std::cout << std::endl;
 }
 
-void print_specification(Specification &s)
+void print_specification(Specification &spec)
 {
-  std::string kind;
-  if (s.type() == typeid(Type_specification)) {
-    print_type_declaration_statement(boost::get<Type_specification>(s));
-  } else if (s.type() == typeid(Parameter_statement)) {
-    //print_parameter_statement(boost::get<Type_specification>(s));
+  if (spec.type() == typeid(Type_specification)) {
+    print_type_declaration_statement(boost::get<Type_specification>(spec));
+  } else if (spec.type() == typeid(Parameter_statement)) {
+    //print_parameter_statement(boost::get<Type_specification>(spec));
   } else {
     std::cout << "unknown specification" << std::endl;
+  }
+}
+
+void print_assignment_statement(Assignment_statement &st)
+{
+  std::cout << "lhs:" << ast::stringize(st.lhs) << std::endl;
+  std::cout << "rhs:" << ast::stringize(st.rhs) << std::endl;
+}
+
+void print_executable_construct(Executable_construct &exec)
+{
+  if (exec.type() == typeid(Assignment_statement)) {
+    print_assignment_statement(boost::get<Assignment_statement>(exec));
+  } else {
+    std::cout << "unknown executable construct" << std::endl;
   }
 }
 
@@ -307,8 +371,11 @@ void print_program(Program &p)
 {
   std::cout << "program name: " << p.name << std::endl;
   std::cout << "specifications:" << std::endl;
-  for(auto s : p.specifications) {
-    print_specification(s);
+  for(auto spec : p.specifications) {
+    print_specification(spec);
+  }
+  for(auto exec : p.executable_constructs) {
+    print_executable_construct(exec);
   }
 }
 
